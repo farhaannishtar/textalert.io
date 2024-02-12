9fff600a8328d22163815eeda8c36c23ad5e6fc0
Farhaan Nishtar <farhaannishtar@gmail.com>
2024-02-12 10:20:29 -0500

    style: adjust layout of input box

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { validatePhoneNumber } from "@/utils/utils";
-// import { createClient } from "@/utils/supabase/server";
+import { validatePhoneNumber } from "@/utils/string-utils";
 import { redirect } from "next/navigation";
 import { useState } from "react";
 import { FaChevronCircleRight } from "react-icons/fa";
@@ -50,7 +49,7 @@ export default function Login({
   };
 
   const [phoneInput, setPhoneInput] = useState("");
-  const [errors, setErrors] = useState([]);
+  const [errors, setErrors] = useState<String[]>([]);
 
   const phoneInputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     const input = e.target.value;
@@ -63,18 +62,19 @@ export default function Login({
   };
 
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2 ">
+    <div className="flex flex-col w-full px-8 mt-10 sm:max-w-md gap-2">
       <form
-        className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground "
+        className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
         action={signIn}
       >
-        <p className="text-3xl  text-black text-center">Sign in or Sign up</p>
+        <p className="text-3xl text-white text-center">Sign in or Sign up</p>
         <div className="relative p-4">
           <input
             type="text"
             value={phoneInput}
             onChange={phoneInputChangeHandler}
-            className="text-sky-900 border-b-slate-800 border-solid border-b-2 p-4 text-2xl pr-4 tracking-widest text-bold "
+            className="text-sky-900 border-solid border-b-2 w-64 sm:w-96 md:w-1/2 lg:w-1/3 xl:w-3/4 p-4 text-lg placeholder-gray-400 text-white pr-4 tracking-widest text-bold"
+            placeholder="Enter Mobile Number"
           />
           <FaChevronCircleRight className="absolute top-8 right-4 text-3xl" />
         </div>
